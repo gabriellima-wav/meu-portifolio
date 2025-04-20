015577923f39d27672c2500f61059bc22e5853b4
gabiellima-wav <seuemail@example.com>
2025-04-20 15:17:22 -0300

    inicio do projeto

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-center">
+      <h1 className="text-3xl font-bold text-blue-600">Portfólio em construção 🚀</h1>
+    </div>
+  )
+}
+
+export default App
